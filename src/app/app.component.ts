b5a40c5d53db1a12a2655303a25df81b8efc41ae
Eric Conner and Josh Gearheart <eric@>
2018-10-16 11:47:16 -0700

    add code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'butt-fart',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'to-do';
+  currentFocus: string = 'to-dos';
+  currentTime = new Date;
+  month: number = this.currentTime.getMonth() + 1;
+  date: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course'),
+    new Task('Begin brainstorming possible JavaScript group projects'),
+    new Task('Add README file to last few Angular repos on GitHub')
+  ];
 }
